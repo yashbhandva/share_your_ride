3e4c4ad5ed084f9fe1c3aee504118c78b66eb839
yashbhandva <yashbhandva01@gmail.com>
2025-12-12 03:27:02 +0530

    Update JwtService.java

--- yavijexpress/yavijexpress/src/main/java/com/yavijexpress/security/JwtService.java
+++ yavijexpress/yavijexpress/src/main/java/com/yavijexpress/security/JwtService.java
@@ -97,7 +97,7 @@ public class JwtService {
     }
 
     private Key getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        byte[] keyBytes = secretKey.getBytes();
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
