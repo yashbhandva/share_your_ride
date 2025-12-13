2c3f7b2c6957f76902fe985f3b3565e81a7fdad7
yashbhandva <yashbhandva01@gmail.com>
2025-12-13 21:10:46 +0530

    Update NotificationRepository.java

--- yavijexpress/yavijexpress/src/main/java/com/yavijexpress/repository/NotificationRepository.java
+++ yavijexpress/yavijexpress/src/main/java/com/yavijexpress/repository/NotificationRepository.java
@@ -1,8 +1,11 @@
 package com.yavijexpress.repository;
 
 import com.yavijexpress.entity.Notification;
+import com.yavijexpress.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Repository
@@ -10,4 +13,8 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     List<Notification> findByUserIdOrderByCreatedAtDesc(Long userId);
     List<Notification> findByUserIdAndIsReadFalse(Long userId);
     Long countByUserIdAndIsReadFalse(Long userId);
+    
+    @Modifying
+    @Transactional
+    void deleteByUser(User user);
 }
