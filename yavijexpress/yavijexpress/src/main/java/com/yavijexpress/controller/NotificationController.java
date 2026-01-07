bb62249d1d085074498c3b9c7c9f6f59f8921ed2
yashbhandva <yashbhandva01@gmail.com>
2026-01-07 12:48:33 +0530

    Update NotificationController.java

--- yavijexpress/yavijexpress/src/main/java/com/yavijexpress/controller/NotificationController.java
+++ yavijexpress/yavijexpress/src/main/java/com/yavijexpress/controller/NotificationController.java
@@ -6,7 +6,6 @@ import com.yavijexpress.entity.Notification;
 import com.yavijexpress.exception.UnauthorizedException;
 import com.yavijexpress.service.NotificationService;
 import com.yavijexpress.utils.SecurityUtils;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
