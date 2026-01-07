25548e4fadf54f1ea77b63c894e6f071df86561a
yashbhandva <yashbhandva01@gmail.com>
2026-01-07 12:49:11 +0530

    Update SecurityConfig.java

--- yavijexpress/yavijexpress/src/main/java/com/yavijexpress/config/SecurityConfig.java
+++ yavijexpress/yavijexpress/src/main/java/com/yavijexpress/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.yavijexpress.config;
 
 import com.yavijexpress.security.JwtAuthenticationFilter;
 import com.yavijexpress.security.LogoutService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
