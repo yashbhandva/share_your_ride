859f088fcd92fbf76c842978ed2d99fd3737fb09
yashbhandva <yashbhandva01@gmail.com>
2026-01-07 12:45:46 +0530

    Update EmergencyAlert.java

--- yavijexpress/yavijexpress/src/main/java/com/yavijexpress/entity/EmergencyAlert.java
+++ yavijexpress/yavijexpress/src/main/java/com/yavijexpress/entity/EmergencyAlert.java
@@ -1,16 +1,10 @@
 package com.yavijexpress.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "emergency_alerts")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class EmergencyAlert {
 
     @Id
