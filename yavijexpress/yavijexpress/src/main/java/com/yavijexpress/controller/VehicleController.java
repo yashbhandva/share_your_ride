df1ccdaef4d5ec3cec665a0b6ff730cd07c43c2e
yashbhandva <yashbhandva01@gmail.com>
2026-01-07 12:47:55 +0530

    Update VehicleController.java

--- yavijexpress/yavijexpress/src/main/java/com/yavijexpress/controller/VehicleController.java
+++ yavijexpress/yavijexpress/src/main/java/com/yavijexpress/controller/VehicleController.java
@@ -3,7 +3,6 @@ package com.yavijexpress.controller;
 import com.yavijexpress.dto.VehicleDTO;
 import com.yavijexpress.service.VehicleService;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
