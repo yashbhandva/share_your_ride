d76f33658fec1ae39455368f5171e2667dcdcc32
yashbhandva <yashbhandva01@gmail.com>
2026-01-07 12:45:08 +0530

    Update Trip.java

--- yavijexpress/yavijexpress/src/main/java/com/yavijexpress/entity/Trip.java
+++ yavijexpress/yavijexpress/src/main/java/com/yavijexpress/entity/Trip.java
@@ -1,18 +1,12 @@
 package com.yavijexpress.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "trips")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Trip {
 
     @Id
