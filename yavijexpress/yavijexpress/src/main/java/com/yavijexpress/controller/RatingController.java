9089269a3e3e7a8b3c81ae0c88edcc61ba4622dc
yashbhandva <yashbhandva01@gmail.com>
2026-01-07 12:48:16 +0530

    Update RatingController.java

--- yavijexpress/yavijexpress/src/main/java/com/yavijexpress/controller/RatingController.java
+++ yavijexpress/yavijexpress/src/main/java/com/yavijexpress/controller/RatingController.java
@@ -3,7 +3,6 @@ package com.yavijexpress.controller;
 import com.yavijexpress.dto.RatingDTO;
 import com.yavijexpress.service.RatingService;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
